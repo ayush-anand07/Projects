8dbc359944f4baaf894ba245af51f4b200e99f10
ayush-anand07 <96003043+ayush-anand07@users.noreply.github.com>
2025-06-04 13:59:46 +0530

    Delete Python Basic/BMI.py

--- Python Basic/BMI.py
+++
@@ -1,9 +0,0 @@
-height = float(input("Enter your height in m : "))
-weight = int(input("Enter your weight in kg : "))
-
-BMI = int(weight/(height**2))
-
-print("BMI is " + str(BMI) + " Kg/sq.m")
-
-
-
